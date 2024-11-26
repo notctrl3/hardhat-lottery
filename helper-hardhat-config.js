4651052b6da31d5ca7f344bbcdd3f9d744117a53
notctrl <1838676259@qq.com>
2024-11-26 18:18:42 +0800

    updates front-end

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,7 +15,7 @@ const networkConfig = {
     },
     11155111: {
         name: "sepolia",
-        subscriptionId: "6926",
+        subscriptionId: "3935",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
@@ -30,8 +30,8 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+const frontEndContractsFile = "../nextjs-smartcontract-lottery/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartcontract-lottery/constants/abi.json"
 
 module.exports = {
     networkConfig,
---
+++ test/staging/Raffle.staging.test.js
@@ -0,0 +1,53 @@
+const { assert, expect } = require("chai")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Staging tests", () => {
+          let raffle, raffleEntranceFee, deployer
+
+          beforeEach(async () => {
+              deployer = (await getNamedAccounts()).deployer
+              raffle = await ethers.getContract("Raffle", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+          })
+
+          describe("fulfillRandomWords", () => {
+              it("works with live Chainlink keepers and Chainlink VRF, we get a random winner", async () => {
+                  const startingTimeStamp = await raffle.getLastTimeStamp()
+                  startingBalance = await ethers.provider.getBalance(raffle.address)
+
+                  await new Promise(async (resolve, reject) => {
+                      // setup listener before we enter the raffle
+                      // Just in case the blockchain moves REALLY fast
+                      raffle.once("WinnerPicked", async () => {
+                          try {
+                              const recentWinner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const endingTimeStamp = await raffle.getLastTimeStamp()
+                              const winnerBalance = await accounts[0].getBalance()
+
+                              await expect(raffle.getPlayer(0)).to.be.reverted
+                              assert.equal(recentWinner.toString(), accounts[0].address)
+                              assert.equal(raffleState, 0)
+                              assert(endingTimeStamp > startingTimeStamp)
+                              assert.equal(
+                                  winnerBalance.toString(),
+                                  startingBalance.add(raffleEntranceFee).toString()
+                              )
+                              resolve() // if try passes, resolves the promise
+                          } catch (e) {
+                              reject(e) // if try fails, rejects the promise
+                          }
+                      })
+                      // Then entering the raffle
+                      console.log("Entering Raffle...")
+                      const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                      await tx.wait(1)
+                      console.log("Ok, time to wait...")
+                      const winnerStartingBalance = await accounts[0].getBalance()
+                  })
+              })
+          })
+      })
